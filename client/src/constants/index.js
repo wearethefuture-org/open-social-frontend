2d152b2125795786521befb5d5a862f76a07772e
ihorzharyi <weathefuture@gmail.com>
2020-05-11 23:28:19 +0300

    update package.json

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -10,3 +10,4 @@ export const SUCCESS_CONFIRM = 'SUCCESS_CONFIRM';
 export const FAIL_CONFIRM = 'FAIL_CONFIRM';
 
 export const apiURL = 'http://178.20.156.208:4000';
+// export const apiURL = 'http://localhost:4000';
