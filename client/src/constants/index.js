c53fad6a01b6c34fb9d7322dc14743e566e74c69
Maxe1 <49842974+Maxe1@users.noreply.github.com>
2021-01-21 21:53:29 +0200

    Update index.js

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -50,4 +50,4 @@ export const SET_FILTER_ANALYTIC = 'SET_FILTER_DATE';
 
 export const apiURL = 'https://social.waf.com.ua';
 // export const apiURL = "http://178.20.156.208:4000";
-export const apiURL = 'http://localhost:4000';
+//export const apiURL = 'http://localhost:4000';
