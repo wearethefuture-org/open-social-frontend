3cc2d922ce774b65a3843100684698d26a7a6f4b
DESKTOP-Q7BB3CC\roman <roman.prystojnyj@gmail.com>
2021-01-21 10:42:20 +0200

    Fixed users reducer

--- client/src/reducers/users.js
+++ client/src/reducers/users.js
@@ -17,7 +17,7 @@ export default (state = initialState, action) => {
     case USERS_DATA_LOADING_DATA_SUCCESS:
       return {
         ...state,
-        data: Array.from(action.payload),
+        data: action.payload,
         error: '',
         isLoading: false,
       };
